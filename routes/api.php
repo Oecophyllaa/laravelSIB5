12c3ec93455b902ddca106f5d8faa3e989969966
Oecophyllaa <rangga.godzila@gmail.com>
2023-12-12 12:36:06 +0700

    auth api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProdukController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +21,16 @@ use Illuminate\Support\Facades\Route;
 // 	return $request->user();
 // });
 
-// PRODUK
-Route::get('/produk', [ProdukController::class, 'index']);
-Route::get('/produk/{id}', [ProdukController::class, 'show']);
-Route::post('/produk-create', [ProdukController::class, 'store']);
-Route::put('/produk/{id}', [ProdukController::class, 'update']);
-Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
+Route::middleware(['auth:sanctum'])->group(function () {
+
+  // PRODUK
+  Route::get('/produk', [ProdukController::class, 'index']);
+  Route::get('/produk/{id}', [ProdukController::class, 'show']);
+  Route::post('/produk-create', [ProdukController::class, 'store']);
+  Route::put('/produk/{id}', [ProdukController::class, 'update']);
+  Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
+});
+
+// AUTH
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
